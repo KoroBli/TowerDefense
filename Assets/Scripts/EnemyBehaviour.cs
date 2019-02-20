2c431c972a5641238beea83ef1f2387b6c7fd135
KoroBli <jordijorba@santmarc.com>
2019-02-20 20:00:52 +0100

    Updated to wave creation

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+    public float speed = 5f;
+
+    private Transform target;
+    private int wavePointIndex = 0;
+
+    void Start()
+    {
+        target = Waypoints.points[0];
+    }
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, target.position) <= 0.4f)
+        {
+            GetNextWayPoint();
+        }
+    }
+
+    void GetNextWayPoint()
+    {
+        if(wavePointIndex >= Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        wavePointIndex++;
+
+        target = Waypoints.points[wavePointIndex];
+    }
+}
---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    private float timeCounter = 2f;
+    private int waveIndex = 0;
+
+
+    void Update()
+    {
+        if(timeCounter <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            timeCounter = timeBetweenWaves;
+        }
+
+        timeCounter -= Time.deltaTime;
+    }
+
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
